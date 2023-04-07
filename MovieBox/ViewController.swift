0cc7810a2aaad755d4aa675bb460d1f872f54744
aslihan-gurkan <aslihangurkan07@gmail.com>
2023-04-07 15:04:58 +0300

    Initial Commit

---
+++ MovieBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieBox
+//
+//  Created by Aslıhan Gürkan on 7.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
